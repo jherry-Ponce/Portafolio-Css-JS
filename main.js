94b958c8f34a817a1ce078417d0d9addd43550a2
jherry-Ponce <jherry_libra23@hotmail.com>
2022-06-17 17:59:03 -0500

    version al 90%

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+window.onload = progresbar;
 const menu = document.querySelector(".burguer");
 menu.addEventListener("click",abrirmenu);
 
@@ -25,4 +26,31 @@ function FuntionResize() {
         const responsive= document.querySelector(".menu-responsive");
         responsive.style.display="none"
     }
+}
+
+
+function progresbar(){
+    const html= document.querySelector("#id100");
+    const progre85=document.querySelector("#id85");
+    const progre80=document.querySelector("#id80");
+    const progre75=document.querySelector("#id75");
+    const progre70=document.querySelector("#id70");
+    const progre50=document.querySelector("#id50");
+    const progre45=document.querySelector("#id45");
+    
+    const bar = document.querySelectorAll(".carga");
+     const bt=Array.from(bar)
+     var width = 1;
+     var identity = setInterval(scene, 10);
+     function scene() {
+       if (width >= 50) {
+         clearInterval(identity);
+       } else {
+         width++; 
+         progre45.style.width = width + '%'; 
+         progre45.innerHTML = width * 1  + '%';
+       }
+      
+    }
+    
 }
